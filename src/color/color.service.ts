7da67c02ccb9c407ac195784835f89923b2fc283
Vladyslav Lubeznyy <vladyslavlubeznyy@MacBook-Pro.local>
2024-03-27 09:05:17 +0200

    add migration for colors slug uniqueness

--- src/color/color.service.ts
+++ src/color/color.service.ts
@@ -9,19 +9,32 @@ export class ColorService {
   constructor(
     @InjectRepository(ColorEntity)
     private readonly colorRepository: Repository<ColorEntity>,
-  ) {}
+  ) {
+    this.migrate();
+  }
 
   findAll() {
     return this.colorRepository.find();
   }
 
-  create(dto: DeepPartial<ColorEntity>) {
-    return this.colorRepository.save(
+  async migrate() {
+    const colors = await this.colorRepository.find();
+    for (const color of colors) {
+      color.slug = `${slugify(color.title, { lower: true })}-${color.id}`;
+      await this.colorRepository.save(color);
+    }
+  }
+
+  async create(dto: DeepPartial<ColorEntity>) {
+    const color = await this.colorRepository.save(
       this.colorRepository.create({
         ...dto,
         slug: slugify(dto.title, { lower: true }),
       }),
     );
+
+    color.slug = `${slugify(dto.title, { lower: true })}-${color.id}`;
+    return this.colorRepository.save(color);
   }
 
   async update(id: number, dto: DeepPartial<ColorEntity>) {
